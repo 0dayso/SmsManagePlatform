6accc223d8c90126f5d2889cc70996ef1c6e9fd0
root <root@localhost.localdomain>
2013-09-06 16:37:23 +0800

    product

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://127.0.0.1/sms/';
+$config['base_url']	= 'http://218.106.126.38:18888/SmsManagePlatform';
 
 /*
 |--------------------------------------------------------------------------
--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'sms';
-$db['default']['password'] = '123456';
+$db['default']['password'] = 'sms';
 $db['default']['database'] = 'sms';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
